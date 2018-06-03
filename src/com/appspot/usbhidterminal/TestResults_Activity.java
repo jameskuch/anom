970d79b2b26022cc19d742ad02e815782fe17ae8
James Kuchenbecker <james.kuchenbecker@gmail.com>
2018-06-02 21:07:17 -0700

    tried to add visualization graph to output

---
+++ src/com/appspot/usbhidterminal/TestResults_Activity.java
@@ -0,0 +1,14 @@
+package com.appspot.usbhidterminal;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TestResults_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_results_);
+    }
+
+}
