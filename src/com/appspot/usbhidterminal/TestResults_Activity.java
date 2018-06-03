a7c77a9fb9b23c357ba06254ebffc4ba2f03c201
James Kuchenbecker <james.kuchenbecker@gmail.com>
2018-06-02 21:17:34 -0700

    got the graph working

--- src/com/appspot/usbhidterminal/TestResults_Activity.java
+++ src/com/appspot/usbhidterminal/TestResults_Activity.java
@@ -12,14 +12,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class TestResults_Activity extends Activity {
 
-    GraphView graph = (GraphView) findViewById(R.id.graph1);
-    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-            new DataPoint(0, 1),
-            new DataPoint(1, 5),
-            new DataPoint(2, 3),
-            new DataPoint(3, 2),
-            new DataPoint(4, 6)
-    });
+
 
 
 
@@ -28,6 +21,15 @@ public class TestResults_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_results_);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph1);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
         graph.addSeries(series);
     }
 
